f0668978173e9d03d23836d8c501c5bb07035a5a
Filinto Duran <filinto@diagrid.io>
2024-11-15 19:29:43 -0600

    update registry/tag

--- package-and-push.sh
+++ package-and-push.sh
@@ -4,7 +4,24 @@ rm -rf ./dapr
 git clone --depth=1 --branch=master git@github.com:dapr/dapr.git
 rm -rf ./dapr/.git
 
-version="v1.15.0-edge"
+version="${IMAGE_TAG:-1.15-master-4-linux-arm64}"
+
+image_registry="${REGISTRY:-ghcr.io/filintod}"
+image_tag="${version}"
+
+echo "Image Registry: ${image_registry}"
+echo "Image Tag: ${image_tag}"
+
+echo "Change Registry (global.registry) to ${image_registry} in ./dapr/charts/dapr/values.yaml"
+echo "Change Image Tag (image.tag) to ${image_tag} in ./dapr/charts/dapr/values.yaml"
+# if on macos
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's|registry:.*|registry: '"${image_registry}"'|' ./dapr/charts/dapr/values.yaml
+    sed -i '' 's|tag:.*|tag: '"${image_tag}"'|' ./dapr/charts/dapr/values.yaml
+else
+    sed -i 's|registry:.*|registry: '"${image_registry}"'|' ./dapr/charts/dapr/values.yaml
+    sed -i 's|tag:.*|tag: '"${image_tag}"'|' ./dapr/charts/dapr/values.yaml
+fi
 
 helm package ./dapr/charts/dapr --version "${version}" --app-version "${version}" -d .
 helm repo index . --url https://filintod.github.io/dapr-charts/
